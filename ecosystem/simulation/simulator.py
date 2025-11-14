d85c304c2043ff33d1b248c84a741e8280594c5b
anandrei90 <andrei90angelescu@gmail.com>
2025-11-14 20:38:22 +0100

    Refactor simulation: remove step size parameter

--- ecosystem/simulation/simulator.py
+++ ecosystem/simulation/simulator.py
@@ -12,12 +12,10 @@ class Simulation:
     High-level simulation controller.
     Responsible for orchestrating environment ticks and controlling runtime.
     """
-    # TODO: get rid of step_size b/c confusing
     def __init__(
         self,
         environment: Environment,
         max_ticks: int = 20,
-        step_size: int = 5,
         verbose: bool = False
     ):
         """
@@ -28,16 +26,13 @@ class Simulation:
         environment: Environment
             Environment to be simulated.
         max_ticks: int
-            Number ot time steps of the simulations.
-        step_size: int
-            I think this parameter is useless...
+            Number ot time steps of the simulations. Must be at least 1.
         verbose: bool
             Controls verbosity of the simulation.
         """
         self.environment = environment
-        self.max_ticks = max_ticks
-        if step_size >= 1:
-            self.step_size = step_size
+        if max_ticks >= 1:
+            self.max_ticks = max_ticks
         else:
             raise ValueError("Step size should be greater than or equal to 1.")
         self.verbose = verbose
@@ -45,6 +40,6 @@ class Simulation:
     def run(self) -> None:
         """Run the simulation until `max_ticks` is reached."""
         while self.environment.tick_count < self.max_ticks:
-            self.environment.run(steps=self.step_size)
+            self.environment.tick()
             if self.verbose:
                 print(f"Tick {self.environment.tick_count}/{self.max_ticks}")
