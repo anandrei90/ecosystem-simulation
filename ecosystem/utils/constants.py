ea2327a10c01e1d294c7d7beed43635da60b11db
anandrei90 <andrei90angelescu@gmail.com>
2026-02-04 18:37:43 +0100

    Add movement directions in constants.py

--- ecosystem/utils/constants.py
+++ ecosystem/utils/constants.py
@@ -2,5 +2,6 @@
 constants.py — collects all constants used in this project.
 """
 
-# Directions in which entities can move (up, down, left, right)
+# Directions in which entities can move (right, left, down, up)
+# Convention: direction = (dx, dy)
 DIRECTIONS = [(1, 0), (-1, 0), (0, -1), (0, 1)]
